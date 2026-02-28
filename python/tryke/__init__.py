a1175059c65e14302ad7eea4ecfcfbaadec3d823
Justin Chapman <commonmodestudio@gmail.com>
2026-02-28 16:31:23 -0500

    skip docs

--- python/tryke/__init__.py
+++ python/tryke/__init__.py
@@ -1,22 +1,15 @@
-"""Python entrypoint for tryke."""
-
 from collections.abc import Callable
 
 
 def test(fn: Callable[[], None]) -> Callable[[], None]:
-    """Test decorator."""
     return fn
 
 
 class Expectation[T]:
-    """Expectation builder."""
-
     def to_equal(self, other: T) -> None:
-        """Assert self value is equal to other."""
         _ = other
 
 
 def expect[T](expr: T) -> Expectation[T]:
-    """Enter assertion logic."""
     _ = expr
     return Expectation()

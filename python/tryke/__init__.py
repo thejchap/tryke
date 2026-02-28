efb9449b737eace460079c7e2c404e24d4ec9a8b
Justin Chapman <commonmodestudio@gmail.com>
2026-02-28 16:21:39 -0500

    typed

--- python/tryke/__init__.py
+++ python/tryke/__init__.py
@@ -3,3 +3,12 @@ from typing import Callable
 
 def test(fn: Callable[[], None]) -> Callable[[], None]:
     return fn
+
+
+class Expectation[T]:
+    def to_equal(self, other: T) -> None:
+        pass
+
+
+def expect[T](expr: T) -> Expectation[T]:
+    return Expectation()
